d0d31bfc33a3c610797e5172da723f1356b5830f
johnpolacek <johnpolacek@gmail.com>
2017-02-12 15:19:26 -0600

    add cancel to create account form

--- js/components/signin.js
+++ js/components/signin.js
@@ -6,9 +6,22 @@ $(function() {
 				.delay(250)
 				.queue(function() {
 					$(this).addClass('hidden').dequeue();
-					next();
 				});
 			$('#createAccountContainer').addClass('transparent').addClass('marg-3-top').delay(301)
+				.queue(function() {
+					$(this).removeClass('marg-3-top').removeClass('hidden').dequeue();
+				}).delay(1).queue(function() {
+					$(this).addClass('marg-3-top-neg').removeClass('transparent').dequeue();
+				});
+		})
+		.on('click','#cancelCreateAccount',function() {
+			$('#createAccountContainer')
+				.addClass('transparent').removeClass('marg-3-top-neg').addClass('marg-3-top')
+				.delay(250)
+				.queue(function() {
+					$(this).addClass('hidden').dequeue();
+				});
+			$('#signInContainer').addClass('transparent').addClass('marg-3-top').delay(301)
 				.queue(function() {
 					$(this).removeClass('marg-3-top').removeClass('hidden').dequeue();
 				}).delay(1).queue(function() {
